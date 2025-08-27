d015f7c7bffe2472f9e9bc5cd2628bbf9ab94d3b
torquatos <226768122+torquatos@users.noreply.github.com>
2025-08-27 10:05:47 -0300

    solving bootcamp exercises

--- exercises_bootcamp_java_riachuelo/riachuelo_java_exercises/src/riachuelo_java_exercises/exerciseOneA.java
+++ exercises_bootcamp_java_riachuelo/riachuelo_java_exercises/src/riachuelo_java_exercises/exerciseOneA.java
@@ -1,5 +1,23 @@
+/*this code is part of the exercise list from the Java Bootcamp 
+ * offered By Riachuelo S/A and Dio.me
+ * 
+ * Escreva um código que receba o nome e o ano de nascimento de alguém e 
+ * imprima na tela a seguinte mensagem: "Olá 'Fulano' você tem 'X' anos"
+ * 
+ * Write a code that will receive the name and the year of birth of the user and
+ * print in the screen the following message: "Hello 'name', you're 'age' years old"
+*/
+
 package riachuelo_java_exercises;
 
+import java.util.*;
+
 public class exerciseOneA {
+	
+	public static void main (String[] args) {
+		var scanner = new Scanner (System.in);
+		System.out.println("Insert your name: ");
+		var name = scanner.next();
+	}
 
 }
