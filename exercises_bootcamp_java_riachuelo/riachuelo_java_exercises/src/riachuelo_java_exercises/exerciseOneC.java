474011d391407f7e3f59535201c4b5679d383b77
torquatos <shauanatorquato@outlook.com>
2025-09-03 11:57:38 -0300

    Update exerciseOneC.java
    answering exercise

--- exercises_bootcamp_java_riachuelo/riachuelo_java_exercises/src/riachuelo_java_exercises/exerciseOneC.java
+++ exercises_bootcamp_java_riachuelo/riachuelo_java_exercises/src/riachuelo_java_exercises/exerciseOneC.java
@@ -3,7 +3,7 @@ Escreva um código que receba a base e a alturade um retângulo, calcule sua ár
 
 fórmula: área=base X altura
 
-Write a code that will receive the base and the high of a rectangle and make the calculation of its area
+Write a code that will receive the base and the hight of a rectangle and make the calculation of its area
 
 formula: area = base * high
 */
@@ -16,11 +16,23 @@ import java.math.*;
 public class exerciseOneC {
 
   public static void main (String[] args) {
+    
     var scanner = new Scanner(System.in);
+    
     System.out.println("Insert the base of your rectangle in centimeters: ");
     
+    double base = scanner.nextDouble();
 
-    
+    System.out.println("Insert the hight of your rectangle in centimeters: ");
+
+    double hight = scanner.nextDouble();
+
+    double areaResult = (base * hight);
+
+    String breakLine = System.lineSeparator();
+
+    System.out.println("For a rectangle which dimensions are " + base + " cm of base and " + hight + " cm of hight, " + breakLine + " its area is " + areaResult + " cm².");
+     
   }
   
 }
