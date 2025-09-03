c4c374a5b4488f1a6f4985af016088d87cf6b302
torquatos <shauanatorquato@outlook.com>
2025-09-03 11:50:47 -0300

    Update exerciseOneC.java
    answering the exercise

--- exercises_bootcamp_java_riachuelo/riachuelo_java_exercises/src/riachuelo_java_exercises/exerciseOneC.java
+++ exercises_bootcamp_java_riachuelo/riachuelo_java_exercises/src/riachuelo_java_exercises/exerciseOneC.java
@@ -10,6 +10,17 @@ formula: area = base * high
 
 package riachuelo_java_exercises;
 
+import java.util.*;
+import java.math.*;
+
 public class exerciseOneC {
 
+  public static void main (String[] args) {
+    var scanner = new Scanner(System.in);
+    System.out.println("Insert the base of your rectangle in centimeters: ");
+    
+
+    
+  }
+  
 }
