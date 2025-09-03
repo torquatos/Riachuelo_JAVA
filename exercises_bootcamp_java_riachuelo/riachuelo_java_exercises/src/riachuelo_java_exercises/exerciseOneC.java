32b987dbf1142d3b698aad7b0c8b47d02b176a68
torquatos <shauanatorquato@outlook.com>
2025-09-03 11:45:16 -0300

    Update exerciseOneC.java
    solving the exercise

--- exercises_bootcamp_java_riachuelo/riachuelo_java_exercises/src/riachuelo_java_exercises/exerciseOneC.java
+++ exercises_bootcamp_java_riachuelo/riachuelo_java_exercises/src/riachuelo_java_exercises/exerciseOneC.java
@@ -1,3 +1,13 @@
+/*
+Escreva um código que receba a base e a alturade um retângulo, calcule sua área e exiba na tela
+
+fórmula: área=base X altura
+
+Write a code that will receive the base and the high of a rectangle and make the calculation of its area
+
+formula: area = base * high
+*/
+
 package riachuelo_java_exercises;
 
 public class exerciseOneC {
